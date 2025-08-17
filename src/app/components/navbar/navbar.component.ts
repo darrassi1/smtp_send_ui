7b63f7e88cbe54920edf40fe4e2f7e17ec34b12e
darrassi1 <younes.darrassi@usmba.ac.ma>
2025-08-17 16:23:14 +0200

    new style for the header

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,10 +7,19 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent {
   isAdmin$ = this.authService.isAdmin$;
+  mobileMenuOpen = false;
 
   constructor(private authService: AuthService) {}
 
   logout(): void {
     this.authService.logout();
+  }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen = false;
   }
 }
